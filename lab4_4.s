66a4e373d478d993fbb83b2f7cd0917b147ab5b2
krishankkrrao <47662287+krishankkrrao@users.noreply.github.com>
2019-04-12 15:16:36 +0530

    Update lab4_4.s

--- lab4_4.s
+++ lab4_4.s
@@ -3,33 +3,33 @@ high:    .space 128                #reserve a block of 128 bytes
 low:   .space 128               
  .text
 main:
-li $t0,-1431655766           # multiplicand
-li $t1,-1431655766           # multiplier
-li $t2,0                     # i counter
-li $t3,32                    # '32-i' counter
-la $s1,low                   # low kaa base adderess
-la $s2,high                  # high kaa base adderess
-li $t4,0                     # carry out
-li $t8,0                     # final high
-li $t9,0                     # final low
+li $t0,-1431655766            
+li $t1,-1431655766           
+li $t2,0                     
+li $t3,32                    
+la $s1,low                  
+la $s2,high                  
+li $t4,0                    
+li $t8,0                     
+li $t9,0                     
 
 
 LOOP1:
-      beq  $t3,$zero,END     # condition check
-      andi $t5,$t1,1         # logical bitwise and
-      beq  $t5,1,LOOP2       # branching condition
-      beq  $t5,0,LOOP3       # branching condition      
+      beq  $t3,$zero,END     
+      andi $t5,$t1,1         
+      beq  $t5,1,LOOP2       
+      beq  $t5,0,LOOP3             
 LOOP2:
-      move  $t6,$t0          # move value $t0 into $t6
-      sllv  $t6,$t6,$t2      # shift left by value of $t2
+      move  $t6,$t0          
+      sllv  $t6,$t6,$t2      
       move  $t7,$t0          
-      srlv  $t7,$t7,$t3      # shift right by value of $t2
+      srlv  $t7,$t7,$t3      
       addu  $t8,$t8,$t6      
       sltu  $t4,$t8,$t6     
       addu  $t9,$t9,$t7
       addu  $t9,$t9,$t4  
-      sw   $t6,0($s1)       #giving data in register $t6 into array
-      sw   $t7,0($s2)       #giving data in register $t7 into array
+      sw   $t6,0($s1)       
+      sw   $t7,0($s2)       
       addi $s1,$s1,4
       addi $s2,$s2,4  
       j COUNTER
